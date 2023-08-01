dcfc5110f2e3afc55d316c6d35ef50b91e239de8
shabeebleo <shabeebpv0786@gmail.com>
2023-08-01 17:57:22 +0530

    database updated

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const state={
     db:null
 }
 module.exports.connect=function(done){
-        const url='mongodb://localhost:27017'
+        const url=process.env.url
         const dbname='AllIndb'
 
         mongoClient.connect(url,(err,data)=>{
